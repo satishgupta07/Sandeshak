65f00709baece38aef8fc4cfd7723f1ee33b5c11
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2026-01-19 22:29:22 +0530

    setup Nativewind (Tailwind CSS)
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-600">
+      <Text className="text-3xl font-bold text-white">
+        NativeWind is Working!
+      </Text>
+    </View>
+  );
+}
